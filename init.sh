2aff0ce0d7d4f2434219842d090a930a0d013a1e
minhluannguyen <minhluannguyen2013@gmail.com>
2024-04-29 22:38:43 +0200

    Refactor code to comment out unused imports and print statements

--- init.sh
+++ init.sh
@@ -1,15 +1,28 @@
+# Create tmp and CA directories
+mkdir -p tmp CA
 
 # CA private key
-openssl ecparam -out ecc.ca.key.pem -name prime256v1 -genkey
+openssl ecparam -out CA/ecc.ca.key.pem -name prime256v1 -genkey
 # CA certificate
-openssl req -config <(printf "[req]\ndistinguished_name=dn\n[dn]\n[ext]\nbasicConstraints=CA:TRUE") -new -nodes -subj "/C=FR/L=Limoges/O=CRYPTIS/OU=SecuTIC/CN=ACSECUTIC" -x509 -extensions ext -sha256 -key ecc.ca.key.pem -text -out ecc.ca.cert.pem
+openssl req -config <(printf "[req]\ndistinguished_name=dn\n[dn]\n[ext]\nbasicConstraints=CA:TRUE") -new -nodes -subj "/C=FR/L=Limoges/O=CRYPTIS/OU=SecuTIC/CN=ACSECUTIC" -x509 -extensions ext -sha256 -key ecc.ca.key.pem -text -out CA/ecc.ca.cert.pem
 
 # Server private key
-openssl ecparam -out ecc.key.pem -name prime256v1 -genkey
+openssl ecparam -out CA/ecc.key.pem -name prime256v1 -genkey
 # Server's Certificate Signing Request
-openssl req -config <(printf "[req]\ndistinguished_name=dn\n[dn]\n[ext]\nbasicConstraints=CA:FALSE") -new -subj "/C=FR/L=Limoges/O=CRYPTIS/OU=SecuTIC/CN=serveur" -reqexts ext -sha256 -key ecc.key.pem -text -out ecc.csr.pem
+openssl req -config <(printf "[req]\ndistinguished_name=dn\n[dn]\n[ext]\nbasicConstraints=CA:FALSE") -new -subj "/C=FR/L=Limoges/O=CRYPTIS/OU=SecuTIC/CN=serveur" -reqexts ext -sha256 -key ecc.key.pem -text -out CA/ecc.csr.pem
 # Sign the CSR with the CA
-openssl x509 -req -days 3650 -CA ecc.ca.cert.pem -CAkey ecc.ca.key.pem -CAcreateserial -extfile <(printf "basicConstraints=critical,CA:FALSE") -in ecc.csr.pem -text -out ecc.serveur.pem
+openssl x509 -req -days 3650 -CA CA/ecc.ca.cert.pem -CAkey CA/ecc.ca.key.pem -CAcreateserial -extfile <(printf "basicConstraints=critical,CA:FALSE") -in ecc.csr.pem -text -out CA/ecc.serveur.pem
 
 # CA public key
-openssl ec -in ecc.ca.key.pem -pubout -out ecc.ca.pub.pem
+openssl ec -in CA/ecc.ca.key.pem -pubout -out CA/ecc.ca.pub.pem
+
+# Get TSA certificates
+if [[ ! -f "CA/tsa.crt" ]]
+then
+	wget https://freetsa.org/files/tsa.crt -P CA
+fi
+
+if [[ ! -f "CA/cacert.pem" ]]
+then
+	wget https://freetsa.org/files/cacert.pem -P CA
+fi
